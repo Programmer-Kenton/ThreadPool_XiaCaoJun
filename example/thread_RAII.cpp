a655e02279de9349d058f8901ef2c1a696fda0e9
Kenton <1441630907@qq.com>
2024-10-09 20:21:51 +0800

    第十一次提交:C++11RAII控制锁lock_guard

--- example/thread_RAII.cpp
+++ example/thread_RAII.cpp
@@ -44,7 +44,40 @@ void TestMutex(int status){
     }
 }
 
+static mutex gmutex;
+void TestLockGuard(int i){
+
+    gmutex.lock();
+    {
+        // adopt_lock的用途
+        // 当已经有一个锁定互斥量的上下文 并且想要在该上下文中使用std::lock_guard来管理这个互斥量的生命周期时
+        // 这种情况 不希望lock_guard再次锁定互斥量 而是希望它接管现有的锁定状态并在std::lock_guard对象的生命周期结束的时候自动释放锁
+        lock_guard<mutex> lock(gmutex,adopt_lock);
+    }
+
+    {
+        lock_guard<mutex> lock(gmutex);
+        cout << "begin thread " << i << endl;
+    }
+
+    for(;;){
+        {
+            lock_guard<mutex> lock(gmutex);
+            cout << "In " << i << endl;
+        }
+        this_thread::sleep_for(500ms);
+    }
+}
+
 int main(int argc,char *argv[]){
+
+    for (int i = 0; i < 3; ++i) {
+        thread th(TestLockGuard,i + 1);
+        th.detach();
+    }
+
+    getchar();
+
     TestMutex(1);
     TestMutex(2);
     getchar();
